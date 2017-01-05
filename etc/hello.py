6bfe81983b9ad81177519fdd008825b1cc691400
IndexOutOfRange404 <alex51@myrambler.ru>
2017-01-05 20:11:49 +0300

    static files v0.7

--- etc/hello.py
+++ etc/hello.py
@@ -1,16 +1,7 @@
 CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/pi/project',
-    #'working_dir': '/home/pi/project/project',
-    'user': 'www-data',
-    'group': 'www-data',
-    'python': '/usr/bin/python',
-    'args': (
-        '--bind=127.0.0.1:8081',
-        '--workers=5', # 5 достаточно для малинки
-        '--graceful-timeout=60',
-        '--timeout=60',
-        #'--debug',
-        #'wsgi:application',
-	'project.wsgi',
-    ),
+    'working_dir': '/home/box/web/',
+    'args': [
+        '-b=0.0.0.0:8080',
+        'hello:app'
+    ]
+}
--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/
+sudo /etc/init.d/nginx restart
+
