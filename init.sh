748c745036e8ecc72308e9c33a68924c70f76113
IndexOutOfRange404 <alex51@myrambler.ru>
2017-01-05 20:13:10 +0300

    static files v0.8

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/
-sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart
 
