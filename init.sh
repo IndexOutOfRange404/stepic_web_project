214f1d983d508a09d71ec9a001195d36ffd9884f
IndexOutOfRange404 <alex51@myrambler.ru>
2017-01-07 23:54:24 +0300

    dynamic files v0.2

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/
-sudo ln -s /home/box/web/etc/ask.e  /etc/gunicorn.d/
+sudo ln -s /home/box/web/etc/ask.py  /etc/gunicorn.d/
 sudo /etc/init.d/gunicorn restart
 
