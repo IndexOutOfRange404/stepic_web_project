5fd4f4dacd85747df179ec362b7b496cce97f3f9
IndexOutOfRange404 <alex51@myrambler.ru>
2017-01-17 23:20:36 +0300

    dynamic files v0.11

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,19 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Question(models.Model):
-    title = models.CharField(max_length=50)
-    text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
-    author = models.ForeignKey(User, related_name='question_author')
-    likes = models.ManyToManyField(User)
-
-
-class QuestionManager:
-    def __init__(self):
-        pass
-
+class QuestionManager(models.Manager):
     @staticmethod
     def new():
         return Question.objects.all().order_by('-pk')[:10]
@@ -22,6 +10,16 @@ class QuestionManager:
     @staticmethod
     def popular():
         return Question.objects.all().order_by('rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, related_name='question_author')
+    likes = models.ManyToManyField(User)
+    objects = QuestionManager()
 
 
 class Answer(models.Model):
