2d978e0087402cd36174f2cd6bb92d5859bd1b7a
IndexOutOfRange404 <alex51@myrambler.ru>
2017-01-17 22:30:26 +0300

    dynamic files v0.4

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    added_at = models.DateField()
+    raiting = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.CommaSeparatedIntegerField(max_length=255)
+
+
+class QuestionManager:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def new():
+        return Question.objects.all().order_by('-pk')[:10]
+
+    @staticmethod
+    def popular():
+        return Question.objects.all().order_by('raiting')
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
--- init.sh
+++ init.sh
@@ -6,4 +6,8 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/
 sudo ln -s /home/box/web/etc/ask.py  /etc/gunicorn.d/
 sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
+sudo mysql -uroot -e "CREATE DATABASE ask;"
+cd ask;
+sudo ./manage.py syncdb
 
